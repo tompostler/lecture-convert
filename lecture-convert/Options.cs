8358a014a0fb2157f11bcf839245cf6cd8356711
Tom Postler <tompostler@outlook.com>
2015-12-07 18:35:26 -0600

    Dir is the only option. Fix #2

--- lecture-convert/Options.cs
+++ lecture-convert/Options.cs
@@ -6,12 +6,6 @@
     {
         public ICollection<LectureInfo> Lectures { get; }
 
-        /// <summary>
-        /// The top level directory to use for execution. A *.txt file containing the lecture 
-        /// information should be within this directory.
-        /// </summary>
-        public string Directory { get; set; }
-
         public bool Die { get; set; }
 
         public Options()
--- lecture-convert/Program.cs
+++ lecture-convert/Program.cs
@@ -19,39 +19,34 @@ namespace UnlimitedInf.LectureConvert
             OptionSet p = new OptionSet
             {
                 "",
-                "Usage: lectureconvert [OPTIONS] INPUTFILE",
+                "Usage: lectureconvert [OPTIONS] INPUTDIR",
                 "Download recorded .mp4 lecture files and convert them into sped up and normalized .mp3s with proper id3 tags for easier on-the-go consumption.",
                 "",
-                "Each line in the input text file for the downloaded recordings should be set up as follows for each line:",
-                "   URL/to/file|AlbumName|TrackTitle",
+                "Each line in the input.txt text file for the downloaded recordings should be set up as follows for each line:",
+                "   URL/to/file|TrackTitle",
                 "",
-                "The resulting filename will be gleaned from the above information, but it does not bother to check if you gave it a sensical name or not. Additionally, track numbering will simply be performed sequentially within the file starting at 1.",
+                "The resulting filename will be gleaned from the above information, and the filename gleaned from TrackTitle will be normalized so there are no offending characters or spaces for the current filesystem. Additionally, track numbering will simply be performed sequentially within the file starting at 1.",
                 "The file name is formed as follows:",
                 "   AlbumName_TrackTitle.mp[3|4]",
                 "",
                 "Options:",
-                {
-                    "dir=|d=",
-                    "The top level directory to use for execution. A input.txt file containing the lecture information should be within this directory instead of specifying INPUTFILE.",
-                    (val) => opts.Directory = val
-                },
                 {
                     "help|h|?",
                     "Print this help text and die.",
                     (val) => opts.Die = true
                 },
                 "",
-                "INPUTFILE:",
+                "INPUTDIR:",
                 "\tThe input file to parse for URLs, file naming, and id3 tag information.",
                 "",
                 "Version: 1.0.0"
             };
 
             // Parse the args
-            List<string> file;
+            List<string> dirs;
             try
             {
-                file = p.Parse(args);
+                dirs = p.Parse(args);
             }
             catch (OptionException e)
             {
@@ -67,12 +62,20 @@ namespace UnlimitedInf.LectureConvert
                 return;
             }
 
-            // See if we're grabbing the input file from dir
-            string inputFile = "";
-            if (Utility.Directory.Exists(opts.Directory))
+            // Make sure we have a dir
+            if (dirs.Count != 1)
             {
-                inputFile = opts.Directory + Path.DirectorySeparatorChar + "input.txt";
-                LectureInfo.Directory = opts.Directory;
+                Utility.Console.Error("Must have one INPUTDIR");
+                p.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+            // Grab the input file from dir
+            string inputFile = "";
+            if (Utility.Directory.Exists(dirs[0]))
+            {
+                inputFile = dirs[0] + Path.DirectorySeparatorChar + "input.txt";
+                LectureInfo.Directory = dirs[0];
                 if (!Utility.File.Exists(inputFile))
                 {
                     inputFile = "";
@@ -80,15 +83,11 @@ namespace UnlimitedInf.LectureConvert
             }
 
             // Make sure we have our input file
-            if (file.Count != 1 && String.IsNullOrEmpty(inputFile))
-            {
-                Utility.Console.Error("Must have one INPUTFILE.");
-                p.WriteOptionDescriptions(Console.Out);
-                return;
-            }
             if (String.IsNullOrEmpty(inputFile))
             {
-                inputFile = file[0];
+                Utility.Console.Error($"Must have an input.txt file in {dirs[0]}");
+                p.WriteOptionDescriptions(Console.Out);
+                return;
             }
 
             // Parse the input file
@@ -112,7 +111,7 @@ namespace UnlimitedInf.LectureConvert
                         string[] lineData = line.Split('|');
                         if (lineData.Length != 3)
                         {
-                            Utility.Console.Error($"Invalid line in INPUTFILE: {line}");
+                            Utility.Console.Error($"Invalid line in input.txt: {line}");
                             p.WriteOptionDescriptions(Console.Out);
                             return;
                         }
